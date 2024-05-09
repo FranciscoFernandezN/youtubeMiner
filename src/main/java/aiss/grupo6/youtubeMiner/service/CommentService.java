649902a06ab1cbea496e91f9882cb758cbd02c71
BPP4634 <beagutara@alum.us.es>
2024-05-09 20:19:24 +0200

    cambiosComentariosDesactivados

--- src/main/java/aiss/grupo6/youtubeMiner/service/CommentService.java
+++ src/main/java/aiss/grupo6/youtubeMiner/service/CommentService.java
@@ -1,13 +1,17 @@
 package aiss.grupo6.youtubeMiner.service;
 
 import aiss.grupo6.youtubeMiner.database.VMComment;
+import aiss.grupo6.youtubeMiner.exception.ChannelNotFoundException;
+import aiss.grupo6.youtubeMiner.exception.InternalErrorException;
 import aiss.grupo6.youtubeMiner.model.Comment;
 import aiss.grupo6.youtubeMiner.model.CommentItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,20 +27,30 @@ public class CommentService {
     @Value( "${api.path}" )
     private String path;
 
+    @Value( "${message.internalError}" )
+    private String internalError;
+
     @Autowired
     RestTemplate restTemplate;
 
-    public List<VMComment> indexCommentsByVideoId(String idVideo, Integer maxComments) throws RestClientException {
+    public List<VMComment> indexCommentsByVideoId(String idVideo, Integer maxComments) throws RestClientException, InternalErrorException {
         List<VMComment> result = new ArrayList<>();
         maxComments = maxComments == null? 10: maxComments;
 
         String uri = path + "/commentThreads?key=" + token + "&videoId=" + idVideo + "&part=snippet&maxResults=" + maxComments;
+        
+        try {
+            ResponseEntity<CommentItem> response = restTemplate.exchange(uri, HttpMethod.GET, null, CommentItem.class);
 
-
-        ResponseEntity<CommentItem> response = restTemplate.exchange(uri, HttpMethod.GET, null, CommentItem.class);
-
-        for(Comment c: response.getBody().getItems()) {
-            result.add(VMComment.of(c));
+            for(Comment c: response.getBody().getItems()) {
+                result.add(VMComment.of(c));
+            }
+        }   catch(HttpClientErrorException e) {
+            if (e.getStatusCode().equals(HttpStatus.FORBIDDEN)) {
+                result = new ArrayList<>();
+            } else {
+                throw new InternalErrorException(internalError);
+            }
         }
 
         return result;
